f40f9578a396c0a359dbedb13f75fbd366dd96aa
Alexander Sharabarov <alcovegan@gmail.com>
2026-08-14 00:37:13 +0300

    chore: release 0.1.1

--- src/routerai/__init__.py
+++ src/routerai/__init__.py
@@ -45,7 +45,7 @@ try:
 
     __version__ = version("routerai")
 except Exception:  # pragma: no cover - source checkout without installed metadata
-    __version__ = "0.1.0"
+    __version__ = "0.1.1"
 
 __all__ = [
     "APIStatusError",
